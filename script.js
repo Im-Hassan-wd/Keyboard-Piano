17733ebce12b5e92d8b745511e886918907f4b72
taiwohusayn5@gmail.com <alhassansahih@gmail.com>
2021-10-04 08:34:40 +0100

    fixing console errs"

--- script.js
+++ script.js
@@ -22,146 +22,162 @@ const tones = [
 
 window.addEventListener("keydown",(e) => {
     const key = e.key; 
-    createNote()
-
-    if(audio.play !== undefined) {
-        audio.play().then(_ => {
-            console.log("play the piano");
-        }).catch(err => {
-            audio.currentTime = 0; 
-        })
-    }
 
     switch (key) {
         case 'a': 
             audio.currentTime = 0; 
             audio.src = tones[0];
             audio.play();
-            break;
+            createNote();
         case 'b':
             audio.currentTime = 0;  
             audio.src = tones[1];
             audio.play();
+            createNote();
             break;
         case 'c':
             audio.currentTime = 0;  
             audio.src = tones[2];
             audio.play();
+            createNote();
             break;
         case 'd':
             audio.currentTime = 0;  
             audio.src = tones[3];
             audio.play();
+            createNote();
             break;
         case 'e':
             audio.currentTime = 0;  
             audio.src = tones[4];
             audio.play();
+            createNote();
             break;
         case 'f':
             audio.currentTime = 0;  
             audio.src = tones[5];
             audio.play();
+            createNote();
             break;
         case 'g':
             audio.currentTime = 0;  
             audio.src = tones[6];
             audio.play();
+            createNote();
             break;
         case 'h':
             audio.currentTime = 0;  
             audio.src = tones[7];
             audio.play();
+            createNote();
             break;
         case 'i':
             audio.src = tones[8];
             audio.play();
+            createNote();
             audio.currentTime = 0;  
             break;
         case 'j':
             audio.currentTime = 0;  
             audio.src = tones[9];
             audio.play();
+            createNote();
             break;
         case 'k':
             audio.currentTime = 0;  
             audio.src = tones[10];
             audio.play();
+            createNote();
             break;
         case 'l':
             audio.currentTime = 0;  
             audio.src = tones[11];
             audio.play();
+            createNote();
             break;
         case 'm':
             audio.currentTime = 0;  
             audio.src = tones[12];
             audio.play();
+            createNote();
             break;
         case 'n':
             audio.currentTime = 0;  
             audio.src = tones[13];
             audio.play();
+            createNote();
             break;
         case 'o':
             audio.currentTime = 0;  
             audio.src = tones[14];
             audio.play();
+            createNote();
             break;
         case 'p':
             audio.currentTime = 0;  
             audio.src = tones[15];
             audio.play();
+            createNote();
             break;
         case 'q':
             audio.currentTime = 0;  
             audio.src = tones[0];
             audio.play();
+            createNote();
             break;
         case 'r':
             audio.currentTime = 0;  
             audio.src = tones[1];
             audio.play();
+            createNote();
             break;
         case 's':
             audio.currentTime = 0;  
             audio.src = tones[2];
             audio.play();
+            createNote();
             break;
         case 't':
             audio.currentTime = 0;  
             audio.src = tones[3];
             audio.play();
+            createNote();
             break;
         case 'u':
             audio.currentTime = 0;  
             audio.src = tones[4];
             audio.play();
+            createNote();
             break;
         case 'v':
             audio.currentTime = 0;  
             audio.src = tones[5];
             audio.play();
+            createNote();
             break;
         case 'w':
             audio.currentTime = 0;  
             audio.src = tones[6];
             audio.play();
+            createNote();
             break;
         case 'x':
             audio.currentTime = 0;  
             audio.src = tones[7];
             audio.play();
+            createNote();
             break;
         case 'y':
             audio.currentTime = 0;  
             audio.src = tones[8];
             audio.play();
+            createNote();
             break;
         case 'z':
             audio.currentTime = 0;  
             audio.src = tones[9];
             audio.play();
+            createNote();
             break;
             
     }
